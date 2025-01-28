cb86961d85eec8b4b50f16dff92bb4b277ca4387
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2025-01-28 18:06:14 +0100

    update

--- src/main/scala/arch/model.scala
+++ src/main/scala/arch/model.scala
@@ -175,11 +175,9 @@ object infra:
 
       def createWallet(id: String): Future[OkResponse | ResultError]
 
-      def addCredit(id: String, value: domain.Credit): Future[
-        OkResponse | ResultError]
+      def addCredit(id: String, value: domain.Credit): Future[OkResponse | ResultError]
 
-      def addDebit(id: String, value: domain.Debit): Future[
-        OkResponse | ResultError]
+      def addDebit(id: String, value: domain.Debit): Future[OkResponse | ResultError]
 
       def getBalance(id: String): Future[domain.Balance | ResultError]
 
