dddff4536082cd0ba0d716850a8ab46cec0f1044
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2025-06-30 21:18:29 +0200

    update

--- src/main/scala/arch/security/serviceSecurityValidator.scala
+++ src/main/scala/arch/security/serviceSecurityValidator.scala
@@ -61,46 +61,6 @@ class ServiceSecurityValidator[A: ValidatorSource](conf: A, client: Client[IO]) 
            error =>
               IO.println(s"===> (not JWKS data) ${error.getMessage}").void
 
-   def validate3(jwtString: String, rolesToCheck: Set[String]): Either[List[(JWTErrors, String)], String] =
-      val valid =
-        jwkSet match
-          case Some(kmap) =>
-            val signedJWT = SignedJWT.parse(jwtString)
-            val kid = signedJWT.getHeader.getKeyID
-            val mbK = kmap.get(kid)
-            mbK match{
-              case Some(publicKey) =>
-                val verifier = new com.nimbusds.jose.crypto.RSASSAVerifier(publicKey.toRSAKey.toRSAPublicKey)
-                val isValid = signedJWT.verify(verifier)
-                val claims = signedJWT.getJWTClaimsSet
-                val now = new Date().getTime / 1000 // current time in seconds
-                val exp = Option(claims.getExpirationTime).map(_.getTime / 1000)
-                val nbf = Option(claims.getNotBeforeTime).map(_.getTime / 1000)
-                val iat = Option(claims.getIssueTime).map(_.getTime / 1000)
-                val isNotExpired = exp.forall(_ > now)
-                val isNotBefore = nbf.forall(_ <= now)
-                val id: String = conf.id(signedJWT.getPayload)
-                val generalChecks = Validated.cond(isValid, id, List((JWTErrors.SignatureNotValid, "JWT signature is not valid")))
-                  .combine(Validated.cond(isNotExpired, id, List((JWTErrors.JWTExpired, "JWT expired"))))
-                  .combine(Validated.cond(isNotBefore, id, List((JWTErrors.JWTNotBefore, "JWT not before"))))
-                if isValid then
-                  val roles = conf.roles(signedJWT.getPayload)
-                  val hasRole = rolesToCheck.subsetOf(roles)
-                  generalChecks.combine(Validated.cond(hasRole,
-                    id,
-                    List((
-                      JWTErrors.RolesInvalid,
-                      s"JWT roles invalid: roles '${rolesToCheck.mkString("{", ", ", "}")}' not in '${
-                        roles.mkString("{", ", ", "}")
-                      }'"))))
-                else
-                  generalChecks
-              case None =>
-                Validated.invalid(List((JWTErrors.KidNotPresentInJWKS, s"Key id '$kid' not match anyone of the present in the JWKS"))) // "left" value
-            }
-          case None       => Validated.invalid(List((JWTErrors.NotJWKS, "Not JWKS"))) // "left" value
-      valid.toEither
-
    def initialize(): Unit =
       import cats.effect.unsafe.implicits.global
       updateJWKS().unsafeRunSync()
